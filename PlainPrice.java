6f61aecfafe69a94634074a70d4749b189f7ac6c
NorhanAbdelkader <norhan.sweed20@gmail.com>
2022-01-01 03:04:50 +0200

    modified

--- PlainPrice.java
+++ PlainPrice.java
@@ -2,7 +2,6 @@ public class PlainPrice implements IDiscount {
 
     @Override
     public double applyDiscount(double originalPrice, Ride ride) {
-        System.out.println("PlainPrice Class:  " + originalPrice);
         return originalPrice;
     }
     
